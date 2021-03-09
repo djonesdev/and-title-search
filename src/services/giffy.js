c649c282eb2eef46fe901e495b53a2fa36d4b2a8
Dom Jones <dominicjones1@dominics-mbp.home>
2021-03-09 15:13:59 +0000

    named object

--- src/services/giffy.js
+++ src/services/giffy.js
@@ -5,7 +5,7 @@ const apiInstance = axios.create({
     timeout: 1000,
 });
 
-export default {
+export const apiService = {
     getGifBySearch: (query) => {
         // TODO: move api key into env vars
         const data = apiInstance.get('gifs/search', { params: { api_key: process.env.REACT_APP_API_KEY, q: query} } )
